45247ff05594f78f8db035e801f7f3c2c67493a6
Le Duong Hien Thong <michaeldiavolos@gmail.com>
2022-06-23 20:46:27 +0000

    code clean up

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,36 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-
-
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
-
-
-
 $(document).ready(function() {
 
   //used to extarct tweet posted database and display on webpage
@@ -174,8 +144,6 @@ $(document).ready(function() {
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
   };
-
-
 })
 
 
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-
+  
+  //function to keep track of word counts and display color red if above 140 char
   $("#tweet-text").on("input", function() {
     let tweetEntered = $("#tweet-text").val()
     let counter = this.parentElement.children[2].children[1].innerHTML
